e8c57b1abdf243f63c2d1f00500457c155a762bf
root <dangvanbinh11012003@gmail.com>
2025-11-29 12:11:39 +0700

    update code 29/11/2025v5

--- resources/js/components/CredentialsTab.jsx
+++ resources/js/components/CredentialsTab.jsx
@@ -254,7 +254,7 @@ const CredentialsTab = () => {
                         <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
                         </svg>
-                        <span>+ Create Credential</span>
+                        <span>Create Credential</span>
                     </button>
                 </div>
             </div>
